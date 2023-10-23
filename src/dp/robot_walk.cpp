ef1517532bf1e5a41a5688d7cf2e37ebcbe3d8af
zhengke <zhengke996@163.com>
2023-10-23 21:28:21 +0800

    add 2023年10月23日21:28:20

---
+++ src/dp/robot_walk.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+ private:
+  int process1(int cur, int rest, int aim, int N) {
+    if (rest == 0) return cur == aim ? 1 : 0;
+    if (cur == 1) return process1(2, rest - 1, aim, N);
+    if (cur == N) return process1(N - 1, rest - 1, aim, N);
+    return process1(cur - 1, rest - 1, aim, N) +
+           process1(cur + 1, rest - 1, aim, N);
+  }
+
+  int process2(int cur, int rest, int aim, int N, vector<vector<int>>& dp) {
+    if (dp[cur][rest] != -1) return dp[cur][rest];
+    // 之前没算过
+    int ans = 0;
+    if (rest == 0)
+      ans = cur == aim ? 1 : 0;
+    else if (cur == 1)
+      ans = process2(2, rest - 1, aim, N, dp);
+    else if (cur == N)
+      ans = process2(N - 1, rest - 1, aim, N, dp);
+    else
+      ans = process2(cur - 1, rest - 1, aim, N, dp) +
+            process2(cur + 1, rest - 1, aim, N, dp);
+
+    dp[cur][rest] = ans;
+    return ans;
+  }
+
+ public:
+  int ways1(int N, int start, int aim, int K) {
+    if (N < 2 || start < 1 || start > N || aim < 1 || aim > N || K < 1)
+      return -1;
+    return process1(start, K, aim, N);
+  }
+
+  int ways2(int N, int start, int aim, int K) {
+    if (N < 2 || start < 1 || start > N || aim < 1 || aim > N || K < 1)
+      return -1;
+
+    vector<vector<int>> dp(N + 1, vector<int>(K + 1, 0));
+    for (int i = 0; i <= N; i++) {
+      for (int j = 0; j <= K; j++) dp[i][j] = -1;
+    }
+    return process2(start, K, aim, N, dp);
+  }
+
+  int ways3(int N, int start, int aim, int K) {
+    if (N < 2 || start < 1 || start > N || aim < 1 || aim > N || K < 1)
+      return -1;
+    vector<vector<int>> dp(N + 1, vector<int>(K + 1, 0));
+    dp[aim][0] = 1;
+    for (int rest = 1; rest <= K; rest++) {
+      dp[1][rest] = dp[2][rest - 1];
+      for (int cur = 2; cur < N; cur++) {
+        dp[cur][rest] = dp[cur - 1][rest - 1] + dp[cur + 1][rest - 1];
+      }
+      dp[N][rest] = dp[N - 1][rest - 1];
+    }
+    return dp[start][K];
+  }
+};
+
+int main() {
+  Solution s;
+  cout << s.ways1(5, 2, 4, 6) << endl;
+  cout << s.ways2(5, 2, 4, 6) << endl;
+  cout << s.ways3(5, 2, 4, 6) << endl;
+  return 0;
+}
