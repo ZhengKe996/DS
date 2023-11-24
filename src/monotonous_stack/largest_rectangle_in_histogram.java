b1b92c9e290eb929a2ffdda384d993ab59a01986
zhengke <zhengke996@163.com>
2023-11-24 15:30:34 +0800

    add 2023年11月24日15:30:33

---
+++ src/monotonous_stack/largest_rectangle_in_histogram.java
@@ -0,0 +1,29 @@
+package monotonous_stack;
+
+import java.util.Stack;
+
+public class largest_rectangle_in_histogram {
+  public int largestRectangleArea(int[] heights) {
+    if (heights == null || heights.length == 0)
+      return 0;
+    int maxArea = 0;
+    Stack<Integer> stack = new Stack<>();
+    for (int i = 0; i < heights.length; i++) {
+      while (!stack.isEmpty() && heights[i] <= heights[stack.peek()]) {
+        int j = stack.pop();
+        int k = stack.isEmpty() ? -1 : stack.peek();
+        int curArea = (i - k - 1) * heights[j];
+        maxArea = Math.max(maxArea, curArea);
+      }
+      stack.push(i);
+    }
+
+    while (!stack.isEmpty()) {
+      int j = stack.pop();
+      int k = stack.isEmpty() ? -1 : stack.peek();
+      int curArea = (heights.length - k - 1) * heights[j];
+      maxArea = Math.max(maxArea, curArea);
+    }
+    return maxArea;
+  }
+}
